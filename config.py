c595199b8131e03cf5854348ba95102fa42d9e44
Mildaxsr <dudchik2007@gmail.com>
2025-01-17 18:06:34 +0500

    123

---
+++ config.py
@@ -0,0 +1,25 @@
+# Параметры окна
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+
+# Цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 102, 204)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+# Настройки платформы
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 15
+PADDLE_SPEED = 7
+
+# Настройки мяча
+BALL_RADIUS = 10
+BALL_SPEED = 5
+
+# Настройки кирпичей
+BRICK_WIDTH = 75
+BRICK_HEIGHT = 30
+BRICK_ROWS = 8
+BRICK_COLS = 11
--- main.py
+++ main.py
@@ -1,40 +1,18 @@
 import pygame
 import sys
 import random
+from config import *
+
 
 # Инициализация Pygame
 pygame.init()
-
-# Параметры окна
-WIDTH, HEIGHT = 800, 600
-FPS = 60
-
-# Цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-BLUE = (0, 102, 204)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-
-# Настройки платформы
-PADDLE_WIDTH = 100
-PADDLE_HEIGHT = 15
-PADDLE_SPEED = 7
-
-# Настройки мяча
-BALL_RADIUS = 10
-BALL_SPEED = 5
-
-# Настройки кирпичей
-BRICK_WIDTH = 75
-BRICK_HEIGHT = 30
-BRICK_ROWS = 10
-BRICK_COLS = 11
+pygame.font.init()  # Инициализация шрифтов
 
 # Создание окна
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Arkanoid")
+pygame.display.set_caption("Game")
 clock = pygame.time.Clock()
+
 
 
 class Paddle:
